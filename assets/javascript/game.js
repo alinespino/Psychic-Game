72f72764877cffe0f18a6fc4b2938b00f49dfe97
Aline Espino <AVE@Alines-MacBook-Air.local>
2017-10-06 22:41:52 -0700

    pseudocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,46 @@
+
+
+
+
+// ( COMPUTER GUESSES ) //
+//Create array of letters and select random letter// 
+
+
+
+// CREATE ID's TO KEEP SCORES
+// create vars to insert scores // 
+
+
+
+
+
+// WINS // 
+// If user clicks on computer guess +1 // 
+//Reset counter if win.
+
+
+
+
+
+// LOSSES //
+// Once Guesses left goes to 0 then +1 // 
+
+
+
+
+//GUESSES LEFT //
+//Start counter on 9 IF letter not guessed do -1 // 
+//Reset counter if 0 lives left
+
+
+
+
+// GUESSES SO FAR // 
+//Show letters clicked by user//
+//Reset if Wins +1 or losses + 1  
+
+
+
+
+
+
